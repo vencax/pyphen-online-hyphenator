905e4e17e53ad923b65fa2597468b70dac64a026
vencax <vencax@centrum.cz>
2018-06-07 12:39:14 +0200

    removeFalseOcurrences

--- server.py
+++ server.py
@@ -2,10 +2,17 @@ from flask import Flask
 from flask.globals import request
 # import base64
 # import datetime
-# import os
+import re
 import pyphen
 
 app = Flask(__name__)
+
+def removeFalseOcurrences(s, alter):
+    """
+    Remove all occurences of hyphen sign surrounded by a space-like char.
+    """
+    return re.sub('\s+(%s)|(%s)\s+' % (alter, alter), ' ', s)
+
 
 @app.route('/', methods=['GET'])
 def hello():
@@ -14,7 +21,7 @@ def hello():
     content = request.args['content']
     dic = pyphen.Pyphen(lang=lang)
     hyphenated = dic.inserted(content, alter)
-    return hyphenated
+    return removeFalseOcurrences(hyphenated, alter)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
